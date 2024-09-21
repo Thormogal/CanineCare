1dc2005d62a010b5e20fab0e0051a562c0d3c113
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2024-09-22 00:04:44 +0200

    Added API integration and hid the API_Key in a separate file

---
+++ src/components/DogDetails.jsx
@@ -0,0 +1,11 @@
+function DogDetails() {
+    // Komponentlogik här
+    return (
+      <div>
+        <h1>Dog Details</h1>
+        {/* Din komponentlogik för att visa detaljer om en hund */}
+      </div>
+    );
+  }
+  
+  export default DogDetails;
