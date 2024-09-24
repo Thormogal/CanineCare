b971fff86a8688605d535122465fc0e1a3ef1706
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2024-09-24 22:27:52 +0200

    Fixed layout even more on the homepage. Added a background. Navbar adjusted. Buttons added. Home adjusted for smaller screens. Title on site sends you back to homepage.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,20 @@ import '../CSS/Home.css';
 function Home() {
   return (
     <div className="home-container">
-    <h1 className="home-title">CanineCare</h1>
-    <p className="home-subtitle">"Where paws get love and tender care"</p>
-      <p className="home-description">
-        At CanineCare, your furry friends are treated like family. We ensure that every wagging tail is happy, and every bark is heard.
-      </p>
+      <div className="home-description">
+        <p className="home-subtitle">
+          <span className="highlight-title">CanineCare</span>: "Where happy paws are everywhere"
+        </p>
+        <p>
+          At CanineCare, your furry friends are treated like family.<br />
+          We ensure that every wagging tail is happy, and every bark is heard.<br /><br />
+          Check out our happy customers in the "Catalog" tab or the left button below.
+  </p>
+</div>
+      <div className="cta-buttons">
+        <a href="/catalog" className="cta-button">View Our Dogs</a>
+        <a href="/contact" className="cta-button cta-secondary">Contact Us</a>
+      </div>
     </div>
   );
 }
