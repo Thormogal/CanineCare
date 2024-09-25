5d520c9d9fbb62e2668fb94bd284d6fec8d9f676
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2024-09-25 15:02:31 +0200

    Changed from browser router to hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home.jsx';
-import Catalog from './components/Catalog.jsx';
-import DogDetails from './components/DogDetails.jsx';
-import Contact from './components/Contact.jsx';
-import AboutUs from './components/AboutUs.jsx';
-import Navbar from './components/Navbar.jsx';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router basename="/CanineCare">
       <Navbar />
       <Routes>
-        <Route path="/CanineCare" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/aboutUs" element={<AboutUs />} />
