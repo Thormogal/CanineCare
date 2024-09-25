4de162ef2004d9abb70bb0e38e7120dbb4dcf4c9
Oskar Lövstrand <Oskar.lovstrand@hotmail.com>
2024-09-25 17:22:51 +0200

    Fixed the routing on the home page buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../CSS/Home.css';
 
 function Home() {
   return (
     <div className="home-container">
       <div className="home-descriptionbox">
-          <span className="home-title">CanineCare</span> <br />
-          <p className="home-subtitle">"Where happy paws are everywhere"
-          </p>
+        <span className="home-title">CanineCare</span> <br />
+        <p className="home-subtitle">"Where happy paws are everywhere"</p>
         <p className="home-text">
           At CanineCare, your furry friends are treated like family.<br />
           We ensure that every wagging tail is happy, and every bark is heard.<br /><br />
           Check out our happy customers in the "Catalog" tab or the left button below.
-  </p>
-</div>
+        </p>
+      </div>
       <div className="cta-buttons">
-        <a href="/catalog" className="cta-button">View Our Dogs</a>
-        <a href="/contact" className="cta-button cta-secondary">Contact Us</a>
+        <Link to="/catalog" className="cta-button">View Our Dogs</Link>
+        <Link to="/contact" className="cta-button cta-secondary">Contact Us</Link>
       </div>
     </div>
   );
